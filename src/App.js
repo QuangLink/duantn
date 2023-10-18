79016ce08624d5e574b6b11bda09149866b35fb1
QuangLink <100298893+QuangLink@users.noreply.github.com>
2023-10-18 17:48:24 +0700

    update

--- src/App.js
+++ src/App.js
@@ -7,28 +7,36 @@ import React from "react";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {apiResponse:""};
+    this.state = { apiResponse: "" };
   }
 
-callAPI(){
-  fetch("http://localhost:9000")
-  .then(res => res.text())
-  .then(res => this.setState({apiResponse:res}));
-}
+  callAPI() {
+    fetch("http://localhost:9000")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
+
   componentWillMount() {
     this.callAPI();
-}
-render() {
-  
-  return (
-    <div className="App">
+  }
 
-      <Navbar />
-      <AllRoutes />
-      <Footer />
-    </div>
-   
-  );
+  render() {
+    const footerStyle = {
+      position: "relative",
+      marginTop:"150px",
+      bottom: 0,
+      left: 0,
+      width: "100%"
+    };
+
+    return (
+      <div className="App">
+        <Navbar />
+        <AllRoutes />
+        <Footer style={footerStyle} />
+      </div>
+    );
+  }
 }
-}
+
 export default App;
